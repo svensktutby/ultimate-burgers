9ec1c199c40d4edc08431d0e05704f4e4f10fe9f
svensktutby <svensk.tut.by@gmail.com>
2021-07-04 21:47:40 +0300

    feat: add @emotion/css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { css } from '@emotion/css';
 
 import { Products } from './Products';
 import { Admin } from './Admin';
 
+const AppStyles = css`
+  width: 380px;
+  margin: 50px auto;
+
+  .Container {
+    padding: 25px;
+    background-color: #1d1e26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+  }
+`;
+
 export const App = () => {
   return (
-    <div>
+    <div className={AppStyles}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
+        <div className="Container">
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
--- src/Products/index.jsx
+++ src/Products/index.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { css } from '@emotion/css';
+
+const ProductsStyles = css`
+  display: flex;
+  flex-direction: column;
+
+  .Logo {
+    width: 125px;
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto 25px;
+  }
+`;
 
 export const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className={ProductsStyles}>
+      <img
+        className="Logo"
+        src="/assets/img/logo.svg"
+        width="125"
+        height="45"
+        alt="Ultimate Burgers"
+      />
+      Products
+    </div>
+  );
 };
